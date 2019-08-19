576138e3005783976bb6a248a8d3e34ae072b79a
denislin <HealMSlin@gmail.com>
2019-08-19 22:25:04 +0800

    apex client

---
+++ client/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <img :src="image" />
+    </header>
+</template>
+
+<script>
+import logoSrc from "@/assets/logo.png"
+
+export default {
+    name: 'Header',
+    data() {
+        return {
+            image: logoSrc
+        };
+    },
+}
+</script>
+
+<style>
+img {
+    width: 300px;
+}
+@media(max-width: 700px) {
+    img {
+        display: block;
+        margin: auto;
+    }
+}
+</style>
---
+++ client/src/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="search">
+        <h1>Track Player Stats</h1>
+        <form v-on:submit.prevent="onSubmit">
+            <div class="form-group">
+                <label for="platform">Platform</label>
+                <select name="platform" id="platform">
+                    <option value="psn">Playstation</option>
+                    <option value="xbl">Xbox</option>
+                    <option value="origin">Origin</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="gamertag">Gamertag</label>
+                <input type="text" name="gamertag" id="gamertag" />
+            </div>
+            <div class="form-group">
+                <input class="btn" type="submit" value="Submit" />
+            </div>
+        </form>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "Search"
+}
+</script>
+
+<style>
+
+</style>
